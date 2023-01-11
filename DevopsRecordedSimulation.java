fdf7506252bcffdd8af130eb541cadf4df8756c4
Raziel Alron <raziel.alron@tipalti.com>
2023-01-11 20:07:32 +0200

    adding load tests

---
+++ DevopsRecordedSimulation.java
@@ -0,0 +1,55 @@
+
+import java.time.Duration;
+import java.util.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+import io.gatling.javaapi.jdbc.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+import static io.gatling.javaapi.jdbc.JdbcDsl.*;
+
+public class DevopsRecordedSimulation extends Simulation {
+
+  {
+    HttpProtocolBuilder httpProtocol = http
+      .baseUrl("http://localhost:8080")
+      .inferHtmlResources()
+    ;
+    
+    Map<CharSequence, String> headers_0 = new HashMap<>();
+    headers_0.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+    headers_0.put("Accept-Encoding", "gzip, deflate, br");
+    headers_0.put("Accept-Language", "en-US,en;q=0.9,he;q=0.8");
+    headers_0.put("Cache-Control", "max-age=0");
+    headers_0.put("Sec-Fetch-Dest", "document");
+    headers_0.put("Sec-Fetch-Mode", "navigate");
+    headers_0.put("Sec-Fetch-Site", "same-origin");
+    headers_0.put("Sec-Fetch-User", "?1");
+    headers_0.put("Upgrade-Insecure-Requests", "1");
+    headers_0.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
+    headers_0.put("sec-ch-ua", "Not?A_Brand\";v=\"8\", \"Chromium\";v=\"108\", \"Google Chrome\";v=\"108");
+    headers_0.put("sec-ch-ua-mobile", "?0");
+    headers_0.put("sec-ch-ua-platform", "Windows");
+
+
+    ScenarioBuilder scn = scenario("DevopsRecordedSimulation")
+      .exec(
+        http("request_0")
+          .get("/ShacharBenShimol-RazielAlron-OmerDavid-TamarBenHillel-OfirRabi/")
+          .headers(headers_0)
+          .resources(
+            http("request_1")
+              .get("/favicon.ico")
+          )
+      );
+
+	setUp(
+		scn.injectOpen(
+			constantUsersPerSec(40).during(120)
+		)
+	)
+	.protocols(httpProtocol);
+  }
+}
